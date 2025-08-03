4b176c1bb0b764cc36ed008f8c407ce3ad13e06c
Hisernberg <nurnabid754@gmail.com>
2025-08-03 15:34:13 +0600

    Add files via upload
    understand Java Variables and Data Types — Learn how to declare variables, assign values, and work with primitive data types like int, float, char, boolean, and more. Based on beginner-friendly W3Schools tutorials.

---
+++ class13.java
@@ -0,0 +1,15 @@
+public class class13 {
+    public static void main(String[] args) {
+        // Good identifier
+        int minutesPerHour = 60;
+
+        // OK, but not descriptive
+        int m = 60;
+
+        // Legal variable name
+        int myInteger = 20;
+        System.out.println(minutesPerHour);
+        System.out.println(m);
+        System.out.println(myInteger);
+    }
+}
---
+++ class14.java
@@ -0,0 +1,29 @@
+public class class14 {
+    public static void main(String[] args) {
+        // Student data
+        String studentName = "John Doe";
+        int studentID = 15;
+        int studentAge = 23;
+        float studentFee = 75.25f;
+        char studentGrade = 'B';
+
+        // Print student variables
+        System.out.println("Student name: " + studentName);
+        System.out.println("Student id: " + studentID);
+        System.out.println("Student age: " + studentAge);
+        System.out.println("Student fee: " + studentFee);
+        System.out.println("Student grade: " + studentGrade);
+
+        // Calculate the Area of a Rectangle
+        int length = 4;
+        int width = 6;
+        int area;
+
+        area = length * width;
+
+        // Print rectangle variables
+        System.out.println("Length is: " + length);
+        System.out.println("Width is: " + width);
+        System.out.println("Area of the rectangle is: " + area);
+    }
+}
